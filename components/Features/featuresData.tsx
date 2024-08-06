2af64b855f7e397a3af52cce1bc4bb77374b7856
Andrey Takhtamirov <andrey1229@yahoo.com>
2024-08-06 10:15:51 -0400

    Initialize project

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Bike-friendly routing",
+    description:
+      "Find routes optimized for cycling, featuring trails, paths, and smaller roads to keep you clear of busy traffic.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "More Modes Tailored for Outdoor Exploration",
+    description:
+      "Choose from three available modes: hiking, cycling, and gravel cycling.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-06.svg",
+    title: "Customizable Options",
+    description:
+      "Adjust your route to be as direct or scenic as you like, balancing efficiency with enjoyment.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-03.svg",
+    title: "Offline Access & Export",
+    description:
+      "Take your journey with you. Save routes to your account or export as GPX.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-04.svg",
+    title: "Area Avoidance",
+    description:
+      "Avoid obstacles like flooded zones or closed roads, so your route adapts to what's happening right now.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-05.svg",
+    title: "Random Round Trip Routing",
+    description:
+      "Not sure where to go? Just pick a distance and let Trailblaze generate a route for you.",
+  }
+];
+
+export default featuresData;
