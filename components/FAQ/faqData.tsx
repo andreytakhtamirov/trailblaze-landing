42cab8e927d573db41679bf30d720e8681a08707
Andrey Takhtamirov <andrey1229@icloud.com>
2024-08-08 16:56:14 -0400

    Add FAQs

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -3,18 +3,18 @@ import { FAQ } from "@/types/faq";
 const faqData: FAQ[] = [
   {
     id: 1,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "Where is Trailblaze available?",
+    ans: "Trailblaze is currently available across all of North America (United States, Canada, and Mexico). Expansion to other regions is planned for the future.",
   },
   {
     id: 2,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "How does Trailblaze compare to other cycling/hiking apps?",
+    ans: "Trailblaze focuses on using trails and quieter roads to offer more enjoyable routes. Additionally, Trailblaze lets you customize the balance between distance and how scenic the route is, allowing for a more personalized and enjoyable riding experience.",
   },
   {
     id: 3,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "Where does Trailblaze get its data?",
+    ans: "Trailblaze uses user-maintained road data from the OpenStreetMap (OSM) initiative.",
   },
 ];
 
