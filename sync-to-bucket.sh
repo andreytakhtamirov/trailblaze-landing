74b76fe92559526c6efe27fba15bef7478803616
Andrey Takhtamirov <andreytakht@gmail.com>
2024-10-30 11:13:05 -0400

    Specify Google Cloud project id

--- sync-to-bucket.sh
+++ sync-to-bucket.sh
@@ -4,6 +4,8 @@ BUCKET_NAME="trailblaze-assets"
 CDN_PATH="_next/static/"
 IMAGE_PATH="public/images/"
 
+gcloud info
+
 # Upload static files
 gsutil -m rsync -r -d .next/static gs://$BUCKET_NAME/$CDN_PATH
 
